2bae0a077129df57f2d574d1f6848fe67ff116b8
Ygor Azambuja <ygorazambuja@gmail.com>
2021-12-05 14:45:35 -0400

    fix: corrige variaveis de ambiente e linter

---
+++ usecases/index.ts
@@ -0,0 +1,5 @@
+import * as documentsUseCase from './document'
+import * as pacientUseCase from './pacient'
+import * as feedbackUseCase from './feedback'
+
+export { documentsUseCase, pacientUseCase, feedbackUseCase }
