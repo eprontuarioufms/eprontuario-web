b389093b18b4b356721facf2b76f1a1f1140accd
Ygor Azambuja <ygorazambuja@gmail.com>
2021-12-05 02:10:21 -0400

    chore: atualiza dependencias

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,9 @@ export default {
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  axios: {
+    baseURL: process.env.API_URL,
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
